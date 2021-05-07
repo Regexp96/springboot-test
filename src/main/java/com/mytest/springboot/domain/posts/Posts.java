8c87bdcc2d4aaef8800c27a979d55e8fed3ae23b
Gyuseok Jung <gsjung@whatap.io>
2021-05-07 15:33:31 +0900

    챕터 3 끝

--- src/main/java/com/mytest/springboot/domain/posts/Posts.java
+++ src/main/java/com/mytest/springboot/domain/posts/Posts.java
@@ -1,6 +1,7 @@
 package com.mytest.springboot.domain.posts;
 
 
+import com.mytest.springboot.domain.BaseTimeEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Builder;
@@ -11,7 +12,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
